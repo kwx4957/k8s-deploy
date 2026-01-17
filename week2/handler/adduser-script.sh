6559f2c171628649f5bcba8f77c7c184a5677780
DongYoung Kim <kwx4957@gmail.com>
2026-01-17 19:05:29 +0900

    week2
    Add functionality to create multiple users with passwords from input arguments.

--- week2/handler/adduser-script.sh
+++ week2/handler/adduser-script.sh
@@ -1,12 +1,28 @@
-# 사용자 추가하는 스크립트 파일 내용 확인
-wget https://raw.githubusercontent.com/naleeJang/Easy-Ansible/refs/heads/main/chapter_07.3/adduser-script.sh
-chmod +x adduser-script.sh
-ls -l adduser-script.sh
+#!/bin/bash
 
-# 해당 스크립트로 user1 생성해보기
-## "passwd: unrecognized option '--stdin'" 에러는 암호 적용 부분에서 발생
-./adduser-script.sh
-./adduser-script.sh "user1" "qwe123"
-tail -n 3 /etc/passwd
-sudo userdel -rf user1
-tail -n 3 /etc/passwd
+# 사용자 계정 및 패스워드가 입력되었는지 확인
+if [[ -n $1 ]] && [[ -n $2 ]]
+then
+
+  UserList=($1)
+  Password=($2)
+
+  # for문을 이용하여 사용자 계정 생성
+  for (( i=0; i < ${#UserList[@]}; i++ ))
+  do
+    # if문을 사용하여 사용자 계정이 있는지 확인
+    if [[ $(cat /etc/passwd | grep ${UserList[$i]} | wc -l) == 0 ]]
+    then
+      # 사용자 생성 및 패스워드 설정
+      useradd ${UserList[$i]}
+      echo ${Password[$i]} | passwd ${UserList[$i]} --stdin
+    else
+      # 사용자가 있다고 메시지를 보여줌.
+      echo "this user ${UserList[$i]} is existing."
+    fi
+  done
+
+else
+  # 사용자 계정과 패스워드를 입력하라는 메시지를 보여줌.
+  echo -e 'Please input user id and password.\nUsage: adduser-script.sh "user01 user02" "pw01 pw02"'
+fi
